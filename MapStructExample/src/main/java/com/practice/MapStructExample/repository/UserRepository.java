5703d06635bb6e4b28779a4c0ed5782ae9e3fc1d
musharaf-fission <musharaf.ahmed@fissionlabs.com>
2022-05-12 16:56:55 +0530

    over

--- MapStructExample/src/main/java/com/practice/MapStructExample/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.practice.MapStructExample.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.practice.MapStructExample.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
